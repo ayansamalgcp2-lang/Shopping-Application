0be8c38f8424db1ba055189cdd7d674734fdad15
ayansamalgcp2-lang <ayansamal.gcp2@gmail.com>
2025-11-18 22:34:48 +0530

    mod-7

--- product-service/src/main/java/com/Spring/microservices/product/dto/ProductRequest.java
+++ product-service/src/main/java/com/Spring/microservices/product/dto/ProductRequest.java
@@ -7,14 +7,22 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+/**
+ * DTO for Product Request
+ * Updated: Testing Docker cache optimization
+ */
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class ProductRequest {
-
+    private static final String VERSION = "1.0.2";    
     private String id;
     private String name;
     private String description;
     private BigDecimal price;
+    public String getVersion() {
+        return VERSION;
+    }
 }
